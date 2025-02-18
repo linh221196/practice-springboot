2ffab43d1e4ef1682918296c7fbc574bd5d51f62
linh221196 <luu.khanh.linh.1034@gmail.com>
2025-02-19 00:28:54 +0900

    GlobalFormatResponse, GlobalException, response customize done

--- src/main/java/com/example/springbootpractice/service/error/GlobalException.java
+++ src/main/java/com/example/springbootpractice/service/error/GlobalException.java
@@ -1,14 +1,21 @@
 package com.example.springbootpractice.service.error;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.springbootpractice.domain.RestResponse;
+
 @RestControllerAdvice
 public class GlobalException {
     @ExceptionHandler(value = IdInvalidException.class)
-    public ResponseEntity<String> handleIdException(IdInvalidException idE){
-        return ResponseEntity.badRequest().body(idE.getMessage());
+    public ResponseEntity<RestResponse<Object>> handleIdException(IdInvalidException idE){
+        RestResponse<Object> res = new RestResponse<Object>();
+                res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+                res.setError(idE.getMessage());
+                res.setMessage("IdInvalidException");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 
 }
