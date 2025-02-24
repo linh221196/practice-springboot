a21551922c36ca64cc151a6ff38cb7d42f2fd28c
linh221196 <luu.khanh.linh.1034@gmail.com>
2025-02-24 15:34:37 +0900

    BaseEntity created

--- src/main/java/com/example/springbootpractice/domain/Companies.java
+++ src/main/java/com/example/springbootpractice/domain/Companies.java
@@ -5,20 +5,15 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.Instant;
 import java.util.List;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import com.example.springbootpractice.domain.entity.BaseEntity;
 
-
-import com.example.springbootpractice.util.SecurityJwt;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Getter
 @Setter
-public class Companies {
+public class Companies extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,33 +22,7 @@ public class Companies {
     @NotBlank(message = "COMPANY'S NAME CANNOT BLANK")
     private String name;
 
-    @CreationTimestamp
-    private Instant createdAt;
-
-    @UpdateTimestamp
-    private Instant updatedAt;
-
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a",timezone = "GMT+7")
-
-  
-    private String createdBy;
-
-
-    private String updatedBy;
-
     @OneToMany (mappedBy = "companies")
     private List<Products> products;
-
-    @PrePersist
-    public void handleBeforeCreate(){
-        String creator = SecurityJwt.getCurrentUserLogin().orElse("");
-        setCreatedBy(creator);
-    }
-
-    @PreUpdate
-    public void handleBeforeUpdate(){
-        String updatePerson = SecurityJwt.getCurrentUserLogin().orElse("");
-        setUpdatedBy(updatePerson);
-    }
 
 }
---
+++ src/main/java/com/example/springbootpractice/domain/entity/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.example.springbootpractice.domain.entity;
+
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.example.springbootpractice.util.SecurityJwt;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter
+@Setter
+public abstract class BaseEntity {
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    private String createdBy;
+    private String updatedBy;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a",timezone = "GMT+7")
+
+    @PrePersist
+    public void prePersist() {
+        String creator =SecurityJwt.getCurrentUserLogin().orElse("system");
+        setCreatedBy(creator);
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedBy = SecurityJwt.getCurrentUserLogin().orElse("system");
+    }
+}
